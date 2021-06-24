ade73030eb2a9375ab3ddd54fafc6a7e09cd68dd
AndreAquilau <andre.aquilau@outlook.com>
2021-06-23 21:39:32 -0400

    chore:
    create screen singin & component button

--- src/screens/signIn/index.tsx
+++ src/screens/signIn/index.tsx
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
-import {View, Text, TextInput} from 'react-native';
+import {View, Image, Text, StatusBar} from 'react-native';
 import { styles } from './styles';
+import IllustrationImg from '../../assets/illustration.png';
+import ButtonIcon from "../../components/ButtonIcon";
 
 export function SignIn(){
-    const model = {
-        input: ''
-    };
 
-    const [getInput, setInput] = useState(model.input);
+    const [text, setText] = useState('');
 
     return (
     <View style={ styles.container }>
-        <Text>Screen SignIn</Text>
-        
-        <TextInput 
-        style={styles.input} 
-        value={getInput} 
-        onChangeText={setInput}
+        <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
         />
+       <Image 
+       source={ IllustrationImg } 
+       style={styles.image} 
+       resizeMode='stretch'
+       ></Image>
 
-        <Text>
-            Você digitou: {getInput}
+       <View style={styles.content}>
+        <Text style={styles.title}>
+            Organize{`\n`}
+            suas jogatinas{`\n`}
+            facilmente{`\n`}
         </Text>
+        <Text style={styles.subtitle}>
+            Crie grupos para jogar seus games {`\n`}
+            favoritos com amigos{`\n`}
+        </Text>
+
+        <ButtonIcon 
+        title='Entre com Discord'
+        activeOpacity={0.7}
+        />
+       </View>
     </View>
     );
 }
--- src/screens/signIn/styles.ts
+++ src/screens/signIn/styles.ts
@@ -1,14 +1,39 @@
 import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: theme.colors.background,
+
     },
     input: {
         height: 38,
         width: 268,
         borderBottomWidth: 2,
+    },
+    image: {
+
+    },
+    content: {
+        width: '100%',
+        height: 360,
+        margin: -40,
+        paddingHorizontal: 50,
+    },
+    title: {
+        color: theme.colors.heading,
+        textAlign: 'center',
+        fontSize: 40,
+        marginBottom: 16,
+    },
+    subtitle: {
+        color: theme.colors.heading,
+        fontSize: 15,
+        textAlign: 'center',
+        marginBottom: 64,  
+        
     }
 })
